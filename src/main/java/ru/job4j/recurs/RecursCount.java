acebef0ada1e6a1ae659846bd5b2b21064316702
aseldom <a.seldom@gmail.com>
2023-09-03 11:58:51 +0600

    first commit

---
+++ src/main/java/ru/job4j/recurs/RecursCount.java
@@ -0,0 +1,18 @@
+package ru.job4j.recurs;
+
+import java.util.Arrays;
+
+public class RecursCount {
+
+    private static int count(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        }
+        return 1 + count(Arrays.copyOfRange(arr, 1, arr.length));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 4, 5};
+        System.out.println(count(arr));
+    }
+}
