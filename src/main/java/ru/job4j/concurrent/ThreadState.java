23776185783a64a3109d75b15a627d9fd88b534f
aseldom <a.seldom@gmail.com>
2023-09-04 17:39:16 +0600

    3. Состояние нити. [#229175 #442313]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,16 +3,17 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> {}
+                () -> { }
         );
         Thread second = new Thread(
-                () -> {}
+                () -> { }
         );
         System.out.println(first.getName());
         System.out.println(second.getName());
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            int a = 0;
         }
         System.out.println("Work ends");
     }
