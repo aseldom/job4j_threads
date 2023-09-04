c0230d1e059dcd6fa0eccf041d1dce5f7347cb61
aseldom <a.seldom@gmail.com>
2023-09-04 20:13:45 +0600

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        int i = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\r load: " + process[i++]);
+                i = i == process.length  ? 0 : i;
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
