1699a8baee6fefc6f98edbc8d43e32e337f64559
aseldom <a.seldom@gmail.com>
2023-09-16 11:02:25 +0600

    temp - Race condition

--- src/main/java/ru/job4j/CountBarrier.java
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -1,11 +1,15 @@
 package ru.job4j;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class CountBarrier {
     private final Object monitor = this;
 
     private final int total;
-
-    private int count;
+    @GuardedBy("this")
+    private int count = 0;
 
     public CountBarrier(final int total) {
         this.total = total;
@@ -14,41 +18,37 @@ public class CountBarrier {
     public void count() {
         synchronized (monitor) {
             count++;
-            notifyAll();
+            monitor.notifyAll();
         }
     }
 
     public void await() {
         synchronized (monitor) {
-            while (count < total) {
+            while (!(count >= total)) {
                 try {
-                    wait();
+                    monitor.wait();
+                    System.out.println(Thread.currentThread().getName() + " try notify");
                 } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+                    throw new RuntimeException(e);
                 }
             }
+            System.out.println(Thread.currentThread().getName() + " notify");
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        CountBarrier barrier = new CountBarrier(3);
-        Thread t1 = new Thread(() -> {
-            String name = Thread.currentThread().getName();
-            System.out.printf("Start %s thread%n", name);
-            barrier.await();
-            System.out.printf("Thread %s finished, count = %s%n", name, barrier.count);
-        }, "First");
-        Thread t2 = new Thread(() -> {
-            String name = Thread.currentThread().getName();
-            System.out.printf("Start %s thread%n", name);
-            barrier.await();
-            System.out.printf("Thread %s finished, count = %s%n", name, barrier.count);
-        }, "Second");
-        t1.start();
-        t2.start();
-        for (int i = 0; i < barrier.total; i++) {
-            barrier.count();
-            Thread.sleep(500);
+    public static void main(String[] args) {
+        CountBarrier countBarrier = new CountBarrier(5);
+        Thread threadAwait = new Thread(countBarrier::await, "Thread-Await");
+        threadAwait.start();
+        for (int i = 1; i <= countBarrier.total; i++) {
+            Thread threadCount = new Thread(countBarrier::count);
+            System.out.println(threadCount.getName());
+            threadCount.start();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
